fb18e78f9f864280250a0fed7473678670e57338
pdokto <96900713+pdokto@users.noreply.github.com>
2022-04-08 10:56:10 +0200

    modulo is love

---
+++ multiple-of-3-or-5.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+function solution(number){
+  let sum=0
+	for (let i=1; i<number; i++){
+    if (i%3===0 || i%5===0) sum+=i
+  }
+  return sum
+}
