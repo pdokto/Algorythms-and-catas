7e993d0ada217ddecbe5fbd2319fae8db791b16d
pdokto <96900713+pdokto@users.noreply.github.com>
2022-04-27 11:20:11 +0200

    Create morse-decode-1.js

---
+++ morse-decode-1.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/54b724efac3d5402db00065e
+
+decodeMorse = function(morseCode){
+  return morseCode.split(" ").map(x => x==""? " " : MORSE_CODE[x])
+                             .join("")
+                             .replace(/ {2,}/g, ' ')
+}
